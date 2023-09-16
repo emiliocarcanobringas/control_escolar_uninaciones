c1805ff9fac2ba252b13b10f7792cc80d892ec55
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2023-09-16 03:55:24 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Desplegable</title>
+    <link rel="stylesheet" href="menu/menu.css">
+</head>
+<body>
+    <select id="menu-desplegable">
+        <option value="inicio">Inicio</option>
+        <option value="constancias">Constancias</option>
+        <option value="ficha-alumno">Ficha del Alumno</option>
+        <option value="planes-estudio">Planes de Estudio</option>
+    </select>
+
+    <div id="panel-contenedor" style="margin-top: 20px; padding: 20px; border: 1px solid #ddd;">
+        <!-- Aquí se cargará el contenido de las páginas -->
+    </div>
+
+    <script src="menu/menu.js"></script>
+</body>
+</html>
